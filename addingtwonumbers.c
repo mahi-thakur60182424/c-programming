b601daa8c414c922c1aebc65fcae40d8a08426e7
mahi thakur <mahithakur6018@gmail.com>
2024-11-04 16:53:06 +0530

    add two numbers

---
+++ addingtwonumbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main(){
+
+ 
+
+    int a;
+    printf("enter a no ");
+    scanf("%d",&a);
+    int b;
+     printf("enter a number ");
+     scanf("%d",&b);
+     int c = a+b;
+      printf("%d",c);
+     return 0;
+ }
