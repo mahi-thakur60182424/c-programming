f102689de991693059b4332de6178cc86ff4181a
mahi thakur <mahithakur6018@gmail.com>
2024-11-07 19:22:17 +0530

    factorials

---
+++ ifelseage.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int age;
+printf("enter a age ");
+scanf("%d",&age);
+ifza(age > 18){
+    printf("adult");
+}
+else{
+    printf("not adult");
+}
+    return 0;
+}
+// done
