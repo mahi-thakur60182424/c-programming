7b15e1597eb1ec3e0a8c7a86bd493aa1a2ca3885
mahi thakur <mahithakur6018@gmail.com>
2024-11-17 08:18:37 +0530

    hololow diamond

---
+++ diamondofnumbers.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5; // Height of the pyramid
+    
+    // Upper part of the diamond
+    for (int i = 1; i <= n; i++) {
+        // Print leading spaces
+        for (int j = i; j < n; j++) {
+            printf(" ");
+        }
+
+        // Print increasing numbers
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Print decreasing numbers
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    // Lower part of the diamond
+    for (int i = n - 1; i >= 1; i--) {
+        // Print leading spaces
+        for (int j = n; j > i; j--) {
+            printf(" ");
+        }
+
+        // Print increasing numbers
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Print decreasing numbers
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ pascaltriangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+    int n = 5; // Number of rows in Pascal's Triangle
+    int coef;  // To store coefficients for each position
+
+    for (int i = 0; i < n; i++) {
+        // Print leading spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            printf(" ");
+        }
+
+        // Calculate and print coefficients
+        coef = 1; // First coefficient is always 1
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", coef);
+            coef = coef * (i - j) / (j + 1); // Calculate next coefficient
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
