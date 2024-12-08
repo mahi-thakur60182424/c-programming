8afb681bee320f22ba3b87d43ac0ce95c998e81f
mahi thakur <mahithakur6018@gmail.com>
2024-12-08 23:08:47 +0530

    adding two numbers (pointers)

---
+++ arrr.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, max, min;
+
+    // Get the number of elements in the array
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input the elements of the array
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Initialize max and min with the first element of the array
+    max = arr[0];
+    min = arr[0];
+
+    // Traverse the array to find max and min
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    // Output the results
+    printf("The largest element is: %d\n", max);
+    printf("The smallest element is: %d\n", min);
+
+    return 0;
+}
